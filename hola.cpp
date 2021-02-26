70d61e73bdb692cbb65f55af982402959bdde404
Nicolas Rojas Martinez <nrojasma@unal.edu.co>
2021-02-26 15:30:26 +0000

    un mensaje cualquiera

---
+++ hola.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //incluir la libreira inpout outpu
+
+//int argc char **argv para lerr argumentos de la linea comando
+int main( int argc, char **argv)
+{
+  std::cout << "Hola mundo!\n";
+
+  return 0;
+}
